2cb0a5b401dab28f5fdd9cd2da0f4e534fb0cb2a
Balraj <bhullar.bs@gmail.com>
2022-09-09 21:31:14 +0530

    missing number prob

---
+++ ds.js
@@ -0,0 +1,26 @@
+//empty array
+//no missing
+const arr = [1,2,3,5,6];
+let missingNum = false
+const searchMissingNum = (arr) => {
+    if(arr.length<1){
+        console.log("empty Array")
+    }
+    for(i=0; i<arr.length; i++){
+        if(arr[i] == i+1){
+
+        }else{
+            missingNum = true
+            return console.log("missing number is: ", i+1)
+        }
+    }
+    if(!missingNum){
+        console.log("nothing is missing")
+    }
+}
+searchMissingNum(arr)
+
+//given array, remove duplicate element of array
+
+let myArr = [0,1,2,3,3,4,4,4,5]
+// output: [0,1,2,3,4,5]
